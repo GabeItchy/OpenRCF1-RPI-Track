4e5b9ff2318e64ce825928a5fa5843094a307f6e
Sulayman Sekandary <102302301+QuackslyAU@users.noreply.github.com>
2023-10-19 11:19:30 +1100

    Update servov2.py
    adds functionality to send information to nginx server on pi 4
    
    probably doesnt work lmao

--- src/servov2.py
+++ src/servov2.py
@@ -1,28 +1,8 @@
 import pigpio
 import pygame
 import time
-import requests
 import socket
-
-
-# Relays server position to pi 4 server
-# TODO: Setup static ip on raspberry pi 4 and find which network to run on. Possible portforward.
-# IP and port of the Raspberry Pi 4 server
-server_ip = '172.20.10.3'
-server_port = 80
-
-# Function to send servo position data to the server
-def send_servo_position(position):
-    data = {"servo_position": pulse_width}
-    try:
-        response = requests.post(f'http://{server_ip}:{server_port}/update_servo', json=data)
-        if response.status_code == 200:
-            print("Servo position sent successfully")
-        else:
-            print("Error sending servo position")
-    except Exception as e:
-        print(f"Error: {e}")
-
+from flask import Flask, request, jsonify    
 
 # Servo Configuration
 servo_gpio_pin = 12
@@ -66,3 +46,66 @@ except KeyboardInterrupt:
 joystick.quit()
 pi.set_servo_pulsewidth(servo_gpio_pin, 0)  # Turn off the servo
 pi.stop()
+
+# Initialize Flask app
+app = Flask(__name)
+
+# Function to send servo position data to the server
+def send_servo_position(position):
+    data = {"servo_position": position}
+    try:
+        response = requests.post(f'http://{server_ip}:{server_port}/update_servo', json=data)
+        if response.status_code == 200:
+            print("Servo position sent successfully")
+        else:
+            print("Error sending servo position")
+    except Exception as e:
+        print(f"Error: {e}")
+
+# API route to update servo position
+@app.route('/update_servo', methods=['POST'])
+def update_servo():
+    data = request.get_json()
+    servo_position = data.get("servo_position")
+    
+    # Update the servo position locally
+    pi.set_servo_pulsewidth(servo_gpio_pin, servo_position)
+    
+    return "Servo position updated", 200
+
+# Function to periodically send servo position to the server
+def send_servo_position_periodically():
+    while True:
+        send_servo_position(pi.get_servo_pulsewidth(servo_gpio_pin))
+        time.sleep(1)
+
+if __name__ == '__main__':
+    # Start the Flask app in a separate thread
+    flask_thread = threading.Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': server_port})
+    flask_thread.daemon = True
+    flask_thread.start()
+
+    # Start the servo update thread
+    servo_update_thread = threading.Thread(target=send_servo_position_periodically)
+    servo_update_thread.daemon = True
+    servo_update_thread.start()
+
+    try:
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.JOYAXISMOTION:
+                    if event.axis == 0:  # Left joystick X-axis
+                        # Map the joystick input (-1 to 1) to servo pulse width and flip the direction
+                        joystick_value = event.value
+                        pulse_width = int(servo_center - joystick_value * servo_range / 2)
+                        pi.set_servo_pulsewidth(servo_gpio_pin, pulse_width)
+                
+                time.sleep(0.01)
+
+    except KeyboardInterrupt:
+        pass
+
+    # Clean up and exit
+    joystick.quit()
+    pi.set_servo_pulsewidth(servo_gpio_pin, 0)  # Turn off the servo
+    pi.stop()
